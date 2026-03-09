bb8b2218c2750b346cc0ec51b0ead1b71a84fee5
Lucas <lucas.sellis@gmail.com>
2026-03-09 10:03:16 -0300

    feat: setup inicial do projeto spring boot

---
+++ src/main/java/com/lucassellis/gerenciamento_produtos_categorias/GerenciamentoDeProdutosECategoriasApplication.java
@@ -0,0 +1,13 @@
+package com.lucassellis.gerenciamento_produtos_categorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeProdutosECategoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeProdutosECategoriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucassellis/gerenciamento_produtos_categorias/GerenciamentoDeProdutosECategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassellis.gerenciamento_produtos_categorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeProdutosECategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
